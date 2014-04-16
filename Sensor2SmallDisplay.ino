8f9d3e5a63f587cea3dc9755f63556f3716d82c2
Tomaz Toplak <tomaz.toplak@gmail.com>
2014-04-16 22:13:29 +0200

    Removed small display code.
    Removed the on-sensor dysplay so both sensor boxes are now equal.

--- Sensor2SmallDisplay.ino
+++ Sensor2SmallDisplay.ino
@@ -33,29 +33,6 @@ const int latchPin[] = {5, 8, 9};
 const int dataPin  = 6;
 const int clockPin = 7; 
 
-const byte numbers[20] = {
-  0b11111100,
-  0b01100000,
-  0b11011010,
-  0b11110010,
-  0b01100110,
-  0b10110110,
-  0b10111110,
-  0b11100000,
-  0b11111110,
-  0b11100110,
-  0b11111101,
-  0b01100001,
-  0b11011011,
-  0b11110011,
-  0b01100111,
-  0b10110111,
-  0b10111111,
-  0b11100001,
-  0b11111111,
-  0b11100111
-};
-
 RF24 radio(4,10);
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xE8E8F0F0E1LL, 0xF0F0F0F0D2LL };
@@ -65,16 +42,6 @@ void setup() {
 
   pinMode(CalibrationLed, OUTPUT);
   pinMode(LaserOutPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-
-  for(int i=0;i<3;i++){    
-    pinMode(latchPin[i], OUTPUT);     
-  }
-
-  displayOut[0]= numbers[10];
-  displayOut[1]= numbers[0];  
-  displayOut[2]= numbers[0]; 
   
   radio.begin();
   radio.setChannel(125);
@@ -169,58 +136,6 @@ void blinkLaser()
   }
 }
 
-void show(byte number[])
-{
-  for(int z=0;z<3;z++){
-    //chariplexing
-    for(int j = 0; j <= 7; j++)
-     {
-       byte toWrite = number[z] & (0b10000000 >> j);
-       shiftIt(toWrite, z);      
-     }
-     
-    //multiplexing
-    //shiftIt(number[z], z);
-  }  
-}
-
-void shiftIt (byte data, int loc)
-{
-  // Set latchPin LOW while clocking these 8 bits in to the register
-  digitalWrite(latchPin[loc], LOW);
-
-  for (int k=0; k <= 7; k++)
-  {
-    // clockPin LOW prior to sending a bit
-    digitalWrite(clockPin, LOW); 
-         
-    if ( data & (1 << k) )
-    {
-      digitalWrite(dataPin, LOW); // turn “On”
-    }
-    else
-    {
-      digitalWrite(dataPin, HIGH); // turn “Off”
-    }
-    // and clock the bit in
-    digitalWrite(clockPin, HIGH);
-  }
-
-  //stop shifting out data
-  digitalWrite(clockPin, LOW); 
-
-  //set latchPin to high to lock and send data
-  digitalWrite(latchPin[loc], HIGH);
-}
-
-void blankDisplay()
-{
-  displayOut[0]= 0;
-  displayOut[1]= 0;
-  displayOut[2]= 0; 
-  show(displayOut);  
-}
-
 boolean isTimeRunning = true;
 boolean doSendTime = false;
 unsigned long lastTeeBrake=0;
@@ -252,28 +167,6 @@ void calculateCurrentTime()
       doSendTime=true;
     }    
   }   
-}
-
-bool showTimeLocal=true;
-void doShowTime()
-{
-  long sec1 = SlideTime/10000;  
-  if(sec1<1)
-  {   
-    displayOut[0]= numbers[(SlideTime/1000)%10+10];
-    displayOut[1]= numbers[(SlideTime/100)%10];  
-    if(!isTimeRunning)
-      displayOut[2]= numbers[(SlideTime/10)%10];
-    else
-      displayOut[2] = 0;
-  }
-  else{
-    displayOut[0]=numbers[sec1];
-    displayOut[1]=numbers[(SlideTime/1000)%10+10];
-    displayOut[2]=numbers[(SlideTime/100)%10];   
-  }
-      
-  show(displayOut);
 }
 
 unsigned long lastRemoteSend = 0;
@@ -324,9 +217,7 @@ void loop() {
     if(got_time[0]==1 || got_time[0]==0){
       Serial.println("DATA: Clock Reply!");  
       
-      waitForResponse = false;  
-      showTimeLocal=false;    
-      blankDisplay();   
+      waitForResponse = false;     
     }  
   }
     
@@ -338,16 +229,10 @@ void loop() {
   //if timeout while sending to remote display
   if(millis()-lastRemoteSend>50 && waitForResponse){
     doSendTime=true;    
-    if(millis()-lastRemoteSend>50)
-      showTimeLocal=true;
   }  
 
   if(doSendTime){ 
     sendTimeToRemoteDisplay();
-  }
-  
-  if(showTimeLocal){
-    doShowTime();
   }
 }
 
